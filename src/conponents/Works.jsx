87062b21b4ba126b0164e4db06be117e69a573f6
Yann <116547223+yannletouzey@users.noreply.github.com>
2023-12-13 13:14:42 +0100

    add logo te replace text link works

--- src/conponents/Works.jsx
+++ src/conponents/Works.jsx
@@ -1,12 +1,15 @@
 import { Link } from 'react-router-dom';
+import netflixLogo from '../assets/img/logo/netflix.png';
+import punchLogo from '../assets/img/logo/punch.png';
+import centroLogo from '../assets/img/logo/centro.png';
 
-const WorksMain = () => {
+const Works = () => {
     return (
         <main className="main">
-            <Link to="/netflix" className="header_title__link">Netflix</Link>
-            <Link to="/punch" className="header_title__link">Punch</Link>
-            <Link to="/centro" className="header_title__link">Centro</Link>
+            <Link to="/netflix" className="header_title__link"><img src={netflixLogo} alt="Logo de Netflix"/></Link>
+            <Link to="/punch" className="header_title__link"><img src={punchLogo} alt="Logo de Punch"/></Link>
+            <Link to="/centro" className="header_title__link"><img src={centroLogo} alt="Logo de Centro"/></Link>
         </main>
     )
 }
-export default WorksMain;
+export default Works;
