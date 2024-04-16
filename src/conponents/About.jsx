e26e7cce4fdf31313e7fd60a3d764cc723c6b0ba
yannletouzey <y.letouzey@gmail.com>
2024-04-16 14:49:39 +0200

    fix setTitleCurrent about me

--- src/conponents/About.jsx
+++ src/conponents/About.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export default function About() {
+export default function About({ setTitleCurrent }) {
+  useEffect(() => {
+    setTitleCurrent("Selim-DM - About me");
+  }, []);
   return (
     <div className="about">
       <h2>Coming soon</h2>
