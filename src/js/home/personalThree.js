686c04b950643e8c935afe8a4a06187479f138a2
yannletouzey <y.letouzey@gmail.com>
2024-06-01 10:34:20 +0200

    fix img three patern + change font

--- src/js/home/personalThree.js
+++ src/js/home/personalThree.js
@@ -8,12 +8,13 @@ const personalThree = [
     { id: 12, img: new URL("../../assets/img/personal/12.jpg", import.meta.url) },
     { id: 19, img: new URL("../../assets/img/personal/19.jpg", import.meta.url) },
     { id: 18, img: new URL("../../assets/img/personal/18.jpg", import.meta.url) },
-    { id: 25, img: new URL("../../assets/img/personal/25.jpg", import.meta.url) },
+    // { id: 25, img: new URL("../../assets/img/personal/25.jpg", import.meta.url) },
+    { id: 40, img: new URL("../../assets/img/personal/40.jpg", import.meta.url) },
     { id: 26, img: new URL("../../assets/img/personal/26.jpg", import.meta.url) },
     { id: 32, img: new URL("../../assets/img/personal/32.jpg", import.meta.url) },
     { id: 37, img: new URL("../../assets/img/personal/37.jpg", import.meta.url) },
-    { id: 40, img: new URL("../../assets/img/personal/40.jpg", import.meta.url) },
-    { id: 36, img: new URL("../../assets/img/personal/36.jpg", import.meta.url) }
+    // { id: 40, img: new URL("../../assets/img/personal/40.jpg", import.meta.url) },
+    // { id: 36, img: new URL("../../assets/img/personal/36.jpg", import.meta.url) }
   ],
   [
     { id: 2, img: new URL("../../assets/img/personal/2.jpg", import.meta.url) },
@@ -24,12 +25,13 @@ const personalThree = [
     { id: 22, img: new URL("../../assets/img/personal/22.jpg", import.meta.url) },
     { id: 21, img: new URL("../../assets/img/personal/21.jpg", import.meta.url) },
     { id: 24, img: new URL("../../assets/img/personal/24.jpg", import.meta.url) },
-    { id: 29, img: new URL("../../assets/img/personal/29.jpg", import.meta.url) },
+    // { id: 29, img: new URL("../../assets/img/personal/29.jpg", import.meta.url) },
+    { id: 36, img: new URL("../../assets/img/personal/36.jpg", import.meta.url) },
     { id: 27, img: new URL("../../assets/img/personal/27.jpg", import.meta.url) },
-    { id: 28, img: new URL("../../assets/img/personal/28.jpg", import.meta.url) },
-    { id: 35, img: new URL("../../assets/img/personal/35.jpg", import.meta.url) },
+    // { id: 28, img: new URL("../../assets/img/personal/28.jpg", import.meta.url) },
     { id: 39, img: new URL("../../assets/img/personal/39.jpg", import.meta.url) },
-
+    { id: 35, img: new URL("../../assets/img/personal/35.jpg", import.meta.url) },
+    // { id: 39, img: new URL("../../assets/img/personal/39.jpg", import.meta.url) },
   ],
   [
     { id: 3, img: new URL("../../assets/img/personal/3.jpg", import.meta.url) },
@@ -40,11 +42,12 @@ const personalThree = [
     { id: 23, img: new URL("../../assets/img/personal/23.jpg", import.meta.url) },
     { id: 17, img: new URL("../../assets/img/personal/17.jpg", import.meta.url) },
     { id: 20, img: new URL("../../assets/img/personal/20.jpg", import.meta.url) },
-    { id: 30, img: new URL("../../assets/img/personal/30.jpg", import.meta.url) },
+    // { id: 30, img: new URL("../../assets/img/personal/30.jpg", import.meta.url) },
+    { id: 38, img: new URL("../../assets/img/personal/38.jpg", import.meta.url) },    
     { id: 31, img: new URL("../../assets/img/personal/31.jpg", import.meta.url) },
     { id: 33, img: new URL("../../assets/img/personal/33.jpg", import.meta.url) },
     { id: 34, img: new URL("../../assets/img/personal/34.jpg", import.meta.url) },
-    { id: 38, img: new URL("../../assets/img/personal/38.jpg", import.meta.url) },    
+    // { id: 38, img: new URL("../../assets/img/personal/38.jpg", import.meta.url) },    
   ]
 ];
 export default personalThree;
